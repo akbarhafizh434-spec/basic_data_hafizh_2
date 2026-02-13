2506053e6ba233233d004e2612bd495983bc0d60
akbarhafizh434-spec <akbarhafizh434@gmail.com>
2026-02-13 16:19:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import pickle
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+import os
 
 # Memuat model dan scaler yang telah disimpan
 @st.cache_resource
